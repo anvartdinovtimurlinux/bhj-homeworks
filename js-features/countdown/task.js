83400eebad1122279d6bd0a7d265d097f55b1a6f
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2019-12-18 20:52:26 +0300

    Первая задача

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,14 +1,27 @@
 'use strict';
 
-const timerField = document.getElementById('timer');
+function transformTimer(timer) {
+  return new Date(timer - 3 * 3600000).toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    hour12: false,
+    minute: 'numeric',
+    second: 'numeric',
+  })
+}
 
 function reduceTimer() {
-  if (timerField.textContent == 0) {
+  if (currentTimer === 0) {
     alert('Вы победили в конкурсе');
     clearInterval(timer);
   } else {
-    timerField.textContent -= 1;
+    currentTimer -= 1000;
+    console.log(currentTimer);
+    timerField.textContent = transformTimer(currentTimer);
   }
 }
+
+const timerField = document.getElementById('timer');
+const [hh, mm, ss] = timerField.innerText.split(':').map(Number);
+let currentTimer = (hh * 3600 + mm * 60 + ss) * 1000;
 
 const timer = setInterval(reduceTimer, 1000);
