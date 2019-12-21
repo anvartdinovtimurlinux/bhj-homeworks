7089a9e2a7fdcc47d029423e345478c22f0414d5
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2019-12-21 17:00:58 +0300

    Домашнее задание к лекции «Способы поиска нужного HTML-элемента» вторая задача

---
+++ element-search/menu/task.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const menus = Array.from(document.querySelectorAll('body > .menu'));
+menus.forEach((menu) => {
+  const menuLinks = Array.from(menu.querySelectorAll('.menu__link'));
+  menuLinks.forEach((item) => {
+    item.onclick = () => {
+      const menuActive = menu.querySelector('.menu_active');
+      if (menuActive) {
+        menuActive.classList.remove('menu_active');
+      }
+
+      const subMenu = item.nextSibling.nextSibling;
+      if (subMenu) {
+        subMenu.classList.add('menu_active');
+        return false
+      }
+
+      return true;
+    };
+  });
+});
