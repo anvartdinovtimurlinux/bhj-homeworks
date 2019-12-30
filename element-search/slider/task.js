eaca3c6ff4e81a6e9be0f366a1959d7a400925b1
anvartdinovtimurlinux <anvartdinovtimurlinux@gmail.com>
2019-12-30 06:01:15 +0300

    Домашнее задание к лекции «Способы поиска нужного HTML-элемента» задача. доработка

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,20 +3,36 @@
 const btnPrevious = document.querySelector('.slider__arrow_prev');
 const btnNext = document.querySelector('.slider__arrow_next');
 const slides = Array.from(document.querySelectorAll('.slider__item'));
+const sliderDots = Array.from(document.querySelectorAll('.slider__dot'));
 let currentSlide = 0;
+sliderDots[currentSlide].classList.add('slider__dot_active');
 
-const changeSlide = (direction) => () =>{
+
+const slideSwitch = (direction) => () => {
   slides[currentSlide].classList.remove('slider__item_active');
+  sliderDots[currentSlide].classList.remove('slider__dot_active');
 
   currentSlide += direction;
   currentSlide = currentSlide === slides.length ? 0 : currentSlide;
   currentSlide = currentSlide === -1 ? slides.length - 1 : currentSlide;
-  
+
   slides[currentSlide].classList.add('slider__item_active');
+  sliderDots[currentSlide].classList.add('slider__dot_active');
 };
 
-const nextSlide = changeSlide(1);
-const previousSlide = changeSlide(-1);
+const slideSwitchByDots = (newSlide) => () => {
+  slides[currentSlide].classList.remove('slider__item_active');
+  sliderDots[currentSlide].classList.remove('slider__dot_active');
 
-btnNext.onclick = nextSlide;
-btnPrevious.onclick = previousSlide;
+  currentSlide = newSlide;
+
+  slides[currentSlide].classList.add('slider__item_active');
+  sliderDots[currentSlide].classList.add('slider__dot_active');
+};
+
+
+btnNext.onclick = slideSwitch(1);
+btnPrevious.onclick = slideSwitch(-1);
+sliderDots.forEach((dot, index) => {
+  dot.onclick = slideSwitchByDots(index);
+});
