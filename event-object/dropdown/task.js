82b404f852b1cf94d51671eb11457ad8a7131ee6
anvartdinovtimurlinux <anvartdinovtimurlinux@gmail.com>
2019-12-30 22:20:39 +0300

    Домашнее задание к лекции «Объект события»  Первая задача

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log('start');
+
+const lists = document.querySelectorAll('.dropdown');
+lists.forEach(list => {
+  const dropdownValue = list.querySelector('.dropdown__value');
+  const dropdownList = list.querySelector('.dropdown__list');
+
+  dropdownValue.addEventListener('click', () => {
+    dropdownList.classList.toggle('dropdown__list_active');
+  });
+
+  const links = dropdownList.querySelectorAll('.dropdown__link');
+  links.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      dropdownValue.textContent = link.textContent;
+      dropdownList.classList.remove('dropdown__list_active');
+    });
+  });
+});
