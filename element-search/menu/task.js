cb61bc0d2c6b4d53c864fb55c5178597b96652e8
anvartdinovtimurlinux <anvartdinovtimurlinux@gmail.com>
2019-12-30 04:59:55 +0300

    Домашнее задание к лекции «Способы поиска нужного HTML-элемента» Вторая задача. доработка

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,19 +2,29 @@
 
 const menus = Array.from(document.querySelectorAll('body > .menu'));
 menus.forEach((menu) => {
-  const menuLinks = Array.from(menu.querySelectorAll('.menu__link'));
-  menuLinks.forEach((item) => {
-    item.onclick = (event) => {
-      const menuActive = menu.querySelector('.menu_active');
-      if (menuActive) {
-        menuActive.classList.remove('menu_active');
-      }
+  let menuLinks = Array.from(menu.querySelectorAll('.menu__link'));
 
-      const subMenu = item.nextSibling.nextSibling;
-      if (subMenu) {
-        subMenu.classList.add('menu_active');
+  menuLinks.forEach((item) => {
+    const subMenu = item.nextSibling.nextSibling;
+
+    if (subMenu) {
+      item.onclick = (event) => {
         event.preventDefault();
-      }
-    };
+
+        if (subMenu.classList.contains('menu_active')) {
+          subMenu.classList.remove('menu_active');
+          return;
+        }
+
+        const menuActive = menu.querySelector('.menu_active');
+        if (menuActive) {
+          menuActive.classList.remove('menu_active');
+        }
+
+        if (subMenu) {
+          subMenu.classList.add('menu_active');
+        }
+      };
+    }
   });
 });
